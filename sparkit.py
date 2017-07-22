3f77784ee523eaef0e65b9e463f1a4ef7659565d
Matt Sparkman <mhsparkman@gmail.com>
2017-07-22 00:23:35 -0500

    Emails will be sent out with the found submission titles. Also added pycache to gitignore

--- sparkit.py
+++ sparkit.py
@@ -1,12 +1,35 @@
+import datetime
 import praw
 import properties
+import pytz
+import smtplib
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(properties.from_email, properties.from_pass)
 
 reddit = praw.Reddit(client_id=properties.client_id,
 					 client_secret=properties.client_secret,
 					 user_agent=properties.user_agent)
-reddit.config._ssl_url = None
 
-print('Read-Only: ' + str(reddit.read_only))
+subreddit = reddit.subreddit(properties.subreddit)
+print("Currently searching r/" + subreddit.display_name + " for \"" + properties.searchPhrase + "\"")
 
-subreddit = reddit.subreddit('learnpython')
-print(subreddit.display_name)
+emailMessage = ""
+for submission in subreddit.hot(limit=10):
+	title = submission.title
+
+	if properties.searchPhrase in title.lower():
+		postDateTime = datetime.datetime.fromtimestamp(submission.created_utc)
+		submissionMessage = submission.title + " | " + postDateTime.strftime("%a %b %d %Y %I:%M:%S %p")
+		print(submissionMessage)
+
+		emailMessage += "\n" + submissionMessage
+
+if (emailMessage != ""):
+	try:
+		server.sendmail(properties.from_email, properties.to_email, emailMessage)
+	except:
+		print("Failed to send email")
+
+server.quit()
