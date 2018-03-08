d8a7708d3bb2fcafccd299dd5364727cfb97c789
Matt Sparkman <mhsparkman@gmail.com>
2018-03-07 21:56:07 -0600

    Started work on allowing to search multiple subreddits. Need to make sure the email gets formatted correctly and also to try and use a dictionary.

--- sparkit.py
+++ sparkit.py
@@ -14,34 +14,52 @@ reddit = praw.Reddit(client_id=properties.client_id,
 					 client_secret=properties.client_secret,
 					 user_agent=properties.user_agent)
 
-subreddit = reddit.subreddit(properties.subreddit)
-print("Currently searching r/" + subreddit.display_name + " for \"" + ",".join(properties.searchPhrases) + "\"")
-
-submissions = subreddit.new(limit=100)
-
-# This should filter out submissions from before the time threshold
-timeThreshold = datetime.utcnow() - timedelta(hours=properties.hoursAgo)
-submissionList = [
-	submission for submission in submissions
-	if (datetime.utcfromtimestamp(submission.created_utc) >= timeThreshold)
-]
-
 emailMessage = ""
-for submission in submissionList:
-	title = submission.title
+for subreddit in properties.subredditList:
+	subreddit = reddit.subreddit(properties.subreddit)
+	print("Currently searching r/" + subreddit.display_name + " for \"" + ",".join(properties.searchPhrases) + "\"")
 
-	for searchPhrase in properties.searchPhrases:
-		# Keep a hashset of submissions found to avoid duplicate rows in the email
-		submissionList = []
+	submissions = subreddit.new(limit=100)
 
-		if searchPhrase.lower() in title.lower() and submission.id not in submissionList:
-			submissionList.append(submission.id)
+	# This should filter out submissions from before the time threshold
+	timeThreshold = datetime.utcnow() - timedelta(hours=properties.hoursAgo)
+	submissionList = [
+		submission for submission in submissions
+		if (datetime.utcfromtimestamp(submission.created_utc) >= timeThreshold)
+	]
 
-			postDateTime = datetime.fromtimestamp(submission.created_utc)
-			print(submission.title + " | " + postDateTime.strftime("%a %b %d %Y %I:%M:%S %p"))
+	if len(submissionList) < 1:
+		continue
 
-			submissionMessage = "<a href=\"" + submission.shortlink + "\">" + submission.title + "</a> | " + postDateTime.strftime("%a %b %d %Y %I:%M:%S %p")
-			emailMessage += "<br />" + submissionMessage
+	'''
+	Try to use a map of subreddits to their specific search criteria
+	subredditSearches = {
+		'motorcycles': [
+			'honda'
+		],
+		'politics': [
+			'trump'
+		]
+	}
+	'''
+
+	emailMessage += "Submissions found for r/" + subreddit + ":<br /> "
+
+	for submission in submissionList:
+		title = submission.title
+
+		for searchPhrase in properties.searchPhrases:
+			# Keep a hashset of submissions found to avoid duplicate rows in the email
+			submissionList = []
+
+			if searchPhrase.lower() in title.lower() and submission.id not in submissionList:
+				submissionList.append(submission.id)
+
+				postDateTime = datetime.fromtimestamp(submission.created_utc)
+				print(submission.title + " | " + postDateTime.strftime("%a %b %d %Y %I:%M:%S %p"))
+
+				submissionMessage = "<a href=\"" + submission.shortlink + "\">" + submission.title + "</a> | " + postDateTime.strftime("%a %b %d %Y %I:%M:%S %p")
+				emailMessage += "<br />" + submissionMessage
 
 if (emailMessage != ""):
 	fromEmail = properties.from_email
